b63457a671c526fee103dfc72691fd74ced71e99
debtman <ip-soft@list.ru>
2019-10-11 18:20:26 +0600

    Просто проверка коммита

---
+++ Multis/Models/MerchUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Multis.Models
+{
+    /*
+     1. Логин*
+2. Юридическое название компании*
+3. Маркетинговое название*
+4. Руководитель фирмы*
+5. Номер телефона руководителя*
+6. Email адрес*
+7. Страна работы*
+8. Город 
+9. Адрес магазина
+10. Номер счета компании
+*/
+    public class MerchUser
+
+    {
+    }
+}
